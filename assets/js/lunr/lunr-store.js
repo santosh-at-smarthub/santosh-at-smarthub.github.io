f3b6eece7d7752cf754773c548c1e1cc3a7d5581
santosh-at-smarthub <santosh-at-smarthub@users.noreply.github.com>
2020-12-24 05:42:39 +0000

    jekyll build from Action 2fbad41b85527af0e24e5e41b53d317c58fb1116

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Release Notes - 2020.09",
+        "excerpt":"Release Name Version SaaS 2020.09-100003 On-premise 3.0.0-100003 Agent 3.0.0-100003 About SmartHub INFER IoT Center SmartHub INFER IoT Center is a secure, enterprise-grade, edge and IoT solution that provides visibility and control over all IoT use cases. It helps companies to onboard, configure, monitor, manage and secure IoT devices and infrastructure....","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/en/INFER-IoT-Center/2020.09/INFER-IoT-Center-202009-Release-Notes.html",
+        "teaser": null
+      },{
+        "title": "Release - 2020.09",
+        "excerpt":"   Release Notes   API Reference Guide   User Guide   Sizing Guide  ","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/en/INFER-IoT-Center/2020.09/",
+        "teaser": null
+      },{
+        "title": "Certified Edge Gateways",
+        "excerpt":"INFER IoT Center is an end-to-end IoT infrastructure management solution that enables both OT and IT staff in organizations to have complete visibility and control of their IoT use cases including onboarding, configuring, monitoring, managing and securing the IoT Edge Systems (gateways) and connected devices. Read Important Support Information: The...","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/en/INFER-IoT-Center/compatibility/certified-edge-gateways",
+        "teaser": null
+      },{
+        "title": "Certified Embedded Systems",
+        "excerpt":"SmartHub INFER is an end-to-end IoT infrastructure management solution that enables both OT and IT staff in organizations to have complete visibility and control of their IoT use cases including onboarding, configuring, monitoring, managing and securing the IoT Embedded Systems and connected devices. Read Important Support Information: The SmartHub INFER...","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/en/INFER-IoT-Center/compatibility/certified-embedded-systems",
+        "teaser": null
+      },{
+        "title": "",
+        "excerpt":"SmartHub INFER IoT Center SmartHub INFER IoT Center is a secure, enterprise-grade IoT device management and monitoring solution. Onboard, manage, monitor and secure IoT use cases from the edge to the cloud, bridge the gap between Information Technology (IT) and Operational Technology (OT) organizations and simplify IoT device management with...","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/",
+        "teaser": null
+      },{
+        "title": "End User License Agreement",
+        "excerpt":"NOTE: THE TERMS OF THIS END USER LICENSE AGREEMENT SHALL GOVERN YOUR USE OF THE SOFTWARE, REGARDLESS OF ANY TERMS THAT MAY APPEAR DURING THE INSTALLATION OF THE SOFTWARE. IMPORTANT-READ CAREFULLY: By downloading, installing, or using the software, you (the individual or legal entity) agree to be bound by the...","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/legal/eula",
+        "teaser": null
+      },{
+        "title": "SmartHub INFER IoT Center 3.0 GA",
+        "excerpt":"The following copyright statements and licenses apply to various open source software packages (or portions thereof) that are distributed with this SmartHub Product. The SmartHub Product may also include other SmartHub components, which may contain additional open source software packages. One or more such open_source_licenses.txt files may therefore accompany this...","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/legal/open_source",
+        "teaser": null
+      },{
+        "title": "Support Policies",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "https://content.smarthub.ai/docs/legal/support-policies",
+        "teaser": null
+      },]
---
+++ docs/legal/pdf_embed_sample.md
@@ -0,0 +1,13 @@
+<!-- ---
+title: "Privacy Policy"
+permalink: "/docs/privacy-policy"
+excerpt: "SmartHub Privacy Policy"
+last_modified_at: September 21, 2020
+toc: false
+---
+
+<object data="/docs/legal/privacy-policy.pdf" type="application/pdf" width="700px" height="700px">
+    <embed src="/docs/legal/privacy-policy.pdf">
+        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="/docs/legal/privacy-policy.pdf">Download PDF</a>.</p>
+    </embed>
+</object> -->
